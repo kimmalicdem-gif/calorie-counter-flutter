c9eb8aed7fcc53af03bcf4989858bd5eb5237e20
Your Name <your.email@gmail.com>
2026-02-10 14:42:07 +0800

    Deploy Wellness at root; keep Calorie Counter at /calorie-counter/

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f0bb68a9119827725b65f4330ca38b85
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "92bf94bae6741db3ff0710df229c2e4f",
+"flutter_bootstrap.js": "d503413d657f09183c2a3337877ee490",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
