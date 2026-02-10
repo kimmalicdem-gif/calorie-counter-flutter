8ad2b3e0e18dbdc5bf359537599fd396f011a35a
Your Name <your.email@gmail.com>
2026-02-10 14:01:48 +0800

    Rebuild web with correct base href for GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f0bb68a9119827725b65f4330ca38b85
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "7ea43ce628f67b0d43554d5f124e5e44",
+"flutter_bootstrap.js": "545dcc9ce2e99eea3e498a6897ff770c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
